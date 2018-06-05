0b88c46527eb29c0b5115a1f0eeccb7c6aa17892
Ameen Solomon <ameens@emeraldlife.co.za>
2018-06-05 11:07:28 +0200

    First page

--- wisdom/models.py
+++ wisdom/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+def job():
+    date_created = models.DateTimeField(verbose_name="Date Listed", blank=False, null=False)
+    decription = models.CharField(blank=False, null=False)

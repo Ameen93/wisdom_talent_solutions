34f9ae3cf6e925cccf17910d162c02444bcfc880
Unknown <ameen.solomon@gmail.com>
2018-06-06 22:19:46 +0200

    bootstrap theme test

--- wisdom/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-def job():
-    date_created = models.DateTimeField(verbose_name="Date Listed", blank=False, null=False)
-    decription = models.CharField(blank=False, null=False)
--- wisdom/views.py
+++ wisdom/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from wisdom_talent_solutions import settings
+
 
 def index(request):
-    return HttpResponse("<h1>You made it to Wisdom</h1>")
+    context = {}
+    context['STATIC_URL']: settings.STATIC_URL
+
+    return render(request, "wisdom/index.html", context)
